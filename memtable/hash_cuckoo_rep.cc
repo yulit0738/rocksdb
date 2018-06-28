2c6527869940728fb05d02b1e9c84821f78bc3ec
yulit0738 <yulit0738@naver.com>
2018-06-28 12:48:53 +0900

    Bug Fixed.

--- memtable/hash_cuckoo_rep.cc
+++ memtable/hash_cuckoo_rep.cc
@@ -471,7 +471,6 @@ namespace rocksdb {
 						auto org = GetSequenceNum(skipp->Key());
 						auto upd = GetSequenceNum(p->Key());
 						if (org > upd) {
-							printf("%zd to %zd\n", org, upd);
 							if (yul_index_skip_array_[job.bucket_id].compare_exchange_weak(skipp, p, std::memory_order_release))
 								break;
 						}
